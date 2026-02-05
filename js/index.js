7958955f3af63fd74dbde9d5c349e1e1cd08e7f5
mendy hershkop <hers.mm@gmail.com>
2026-02-05 15:59:08 +0200

    logic simplae player

--- js/index.js
+++ js/index.js
@@ -4,9 +4,39 @@ const cube1 = document.getElementById('cube1');
 const cube2 = document.getElementById('cube2');
 const roll = document.getElementById('roll');
 const hold = document.getElementById('hold');
+const allScore = document.getElementById('p1_jeneral');
+const tempScore = document.getElementById('p1_current');
+   
+let count= 0 
 
 function rollCube() {
         cube1.innerText = Math.floor(Math.random()*6)+1;
         cube2.innerText = Math.floor(Math.random()*6)+1;
-        console.log(cube1, cube2);
-}
+        count+=1;
+     
+        if (!(count ===5 || cube1.textContent == cube2.textContent)){
+            addScore(Number(cube1.textContent) + Number(cube2.textContent))    
+        }
+        if(cube1.textContent == cube2.textContent){
+            roll.disabled =true
+            tempScore.innerText = 0
+        }
+        else if(count ===5){
+        roll.disabled =true
+        addScore(Number(cube1.textContent) + Number(cube2.textContent))
+        }
+    }
+    
+    function addScore(scure){
+        
+        
+        tempScore.innerText = Number(tempScore.textContent)+scure;   
+        
+    }
+    hold.addEventListener('click', ()=>{
+        
+        count=0
+        roll.disabled = false;
+        allScore.innerHTML = Number(allScore.textContent)+Number(tempScore.textContent)
+        tempScore.innerText = 0
+})
